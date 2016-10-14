120f5b37e50b4f0744f9d82d34ace3891771d99d
saer alyasso <saer.alyasso@nackademin.se>
2016-10-14 19:20:50 +0200

    ResultFromInput class is green now. Test completed

--- src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
@@ -12,5 +12,26 @@ public class ResultsFromInputsTest {
 		resultsFromInputs.setResultForNameLenght(10);
 		assertEquals("The result should be 10", resultsFromInputs.getResultForNameLenght(), 10);
 	}
+	
+	@Test
+	public void testGetResultForGender(){
+		ResultFromInputs result = new ResultFromInputs();
+		result.setResultForGender(5);
+		assertEquals("The result should be 5",result.getResultForGender(),5);
+	}
+	
+	@Test
+	public void testGetResultForAge(){
+		ResultFromInputs result = new ResultFromInputs();
+		result.setResultForAge(20);
+		assertEquals("The result should be 20",result.getResultForAge(),20);
+	}
+	
+	@Test
+	public void testGetResultForHomeCity(){
+		ResultFromInputs result = new ResultFromInputs();
+		result.setResultForHomeCity(7);
+		assertEquals("The result should be 7",result.getResultForHomeCity(),7);
+	}
 
 }
